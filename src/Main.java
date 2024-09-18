a8893852c2fef48c9998ee24a9fa4324f948b00d
jose jimenez <jimenjos000@warren.k12.in.us>
2024-09-18 13:41:22 -0400

    fgbhgds

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(getDaysInMonth(1, 2020));
+        System.out.println(getDaysInMonth(2, 2020));
+        System.out.println(getDaysInMonth(2, 2018));
+        System.out.println(getDaysInMonth(-1, 2020));
+        System.out.println(getDaysInMonth(2, -2020));
+    }
+
+    private static boolean isLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+
+                } else {
+                    return false;
+                }
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
+
+    public static int getDaysInMonth(int month, int year) {
+        if (year < 1 || year > 9999) {
+            return -1;
+        }
+
+        // TRADITIONAL VERSION
+        switch (month) {
+            case 1:
+                return 31;
+            case 2:
+                if (isLeapYear(year)) {
+                    return 29;
+                }
+                return 28;
+            // TODO: YOU do the rest for 3 - 12
+            default:
+                return -1;
+
+
+        }
+
+    }
 }
+
